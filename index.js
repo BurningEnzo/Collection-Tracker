7aaa4458b771ba434779b18183648641e2843486
Lorenzo Tomás <ltomas87@uoc.edu>
2022-04-12 20:36:50 +0200

    Port test

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/api/notes/:id', (request, response) => {
     response.json(note)
   })
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  const PORT = process.env.PORT || 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
